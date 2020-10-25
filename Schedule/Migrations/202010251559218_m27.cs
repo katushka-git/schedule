b7aef300deb509680e0d8f8bd8c691c95071c842
katya.trajxel <katya.trajxel@mail.ru>
2020-10-25 18:50:46 +0200

    all ok

---
+++ Schedule/Migrations/202010251559218_m27.cs
@@ -0,0 +1,16 @@
+﻿namespace Schedule.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class m27 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
